acc5dbf37dbfb44bb78668872f706d57e39a9997
Michael Killough <michaeljkillough@gmail.com>
2018-02-25 19:49:59 +0000

    Add tests for handlers.

--- src/handler.rs
+++ src/handler.rs
@@ -70,3 +70,70 @@ where
         (self.0)(req, resp).into_response()
     }
 }
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use futures::{self, Future, Stream};
+
+    fn sync_handler_returning_response(
+        _req: &mut http::Request<SyncBody>,
+        mut resp: ResponseBuilder,
+    ) -> http::Response<SyncBody> {
+        resp.body(SyncBody::empty()).unwrap()
+    }
+
+    fn sync_handler_returning_result(
+        _req: &mut http::Request<SyncBody>,
+        mut resp: ResponseBuilder,
+    ) -> http::Response<SyncBody> {
+        resp.body(SyncBody::empty()).unwrap()
+    }
+
+    fn async_handler(
+        _req: &mut http::Request<AsyncBody>,
+        mut resp: ResponseBuilder,
+    ) -> FutureResponse<AsyncBody> {
+        let resp = resp.body(AsyncBody::empty()).unwrap();
+        Box::new(futures::future::ok(resp))
+    }
+
+    fn request<Body>() -> http::Request<Body>
+    where
+        Body: EmptyBody,
+    {
+        http::Request::builder().body(Body::empty()).unwrap()
+    }
+
+    #[test]
+    fn test_sync_handler_func_returning_response() {
+        let handler = SyncHandlerFunc(sync_handler_returning_response);
+        let resp = handler.handle(&mut request(), ResponseBuilder::new());
+        assert_eq!(resp.body(), &SyncBody::empty());
+    }
+
+    #[test]
+    fn test_sync_handler_func_returning_result() {
+        let handler = SyncHandlerFunc(sync_handler_returning_result);
+        let resp = handler.handle(&mut request(), ResponseBuilder::new());
+        assert_eq!(resp.body(), &SyncBody::empty());
+    }
+
+    #[test]
+    fn test_sync_handler_closure() {
+        let handler = SyncHandlerFunc(|req, resp| sync_handler_returning_response(req, resp));
+        let resp = handler.handle(&mut request(), ResponseBuilder::new());
+        assert_eq!(resp.body(), &SyncBody::empty());
+    }
+
+    #[test]
+    fn test_async_handler() {
+        let handler = AsyncHandlerFunc(async_handler);
+        let fut = handler.handle(&mut request(), ResponseBuilder::new());
+        let resp = fut.wait().unwrap();
+        let (_, body_stream) = resp.into_parts();
+        let body = body_stream.concat2().wait().unwrap().to_vec();
+        assert_eq!(body, vec![],);
+    }
+}
