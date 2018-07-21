a3dce5e5545868d0ef70e2ed00d6aaf13b0837df
Michael Killough <michaeljkillough@gmail.com>
2018-07-21 20:41:22 +0100

    Update static-files example.

--- examples/static-files.rs
+++ examples/static-files.rs
@@ -6,7 +6,9 @@ use aitch::servers::hyper::Server;
 use aitch::{middlewares, Result};
 
 fn main() -> Result<()> {
-    let handler = static_files_handler("./")?;
+    // NOTE: this expects to be run from the root of the repository.
+    // The folder contains an index.html: browse to http://localhost:3000/index.html
+    let handler = static_files_handler("./examples/static-files/")?;
     let wrapped = middlewares::with_stdout_logging(handler);
 
     let addr = "127.0.0.1:3000".parse()?;
